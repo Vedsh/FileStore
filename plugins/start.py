9acad7a1dbe0b36976bfad2f8e911f8e78b69c8b
Vedsh <79149371+Vedsh@users.noreply.github.com>
2024-04-20 11:04:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,6 +15,9 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
+"""add time im seconds for waitingwaiting before delete 
+1min=60, 2min=60×2=120, 5min=60×5=300"""
+SECONDS = int(os.getenv("SECONDS", "300"))
 
 
 
