60d5b129ce62a487c394565e8b15f37998b21a44
Vedsh <79149371+Vedsh@users.noreply.github.com>
2024-04-22 12:13:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 """add time im seconds for waitingwaiting before delete 
 1min=60, 2min=60×2=120, 5min=60×5=300"""
-SECONDS = int(os.getenv("SECONDS", "30"))
+SECONDS = int(os.getenv("SECONDS", "60"))
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
@@ -89,7 +89,7 @@ async def start_command(client: Client, message: Message):
                 snt_msgs.append(snt_msg)
             except:
                 pass
-        SD = await message.reply_text("Baka! Files Will Be Deleted After 30 Seconds.. Save Them To Saved Messages Now !! Immediately !!")
+        SD = await message.reply_text("Baka! Files Will Be Deleted After 60 Seconds.. Before Download Save Them To Saved Messages Now or Somewhere Else Then Download The Files !! Immediately !!")
         await asyncio.sleep(SECONDS)
 
         for snt_msg in snt_msgs:
